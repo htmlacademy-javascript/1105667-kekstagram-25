1ef04d7ce8d4f1d2f35a92c0fb558dddddc27571
Senya2648 <printsksenia@gmail.com>
2022-02-20 15:21:03 +0500

    Нужно больше функций

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+let getRandomNumber = function (min, max) {
+  if (min >=0 && max>min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  return 'Ошибка. Неправильно заданный диапазон';
+};
+
+getRandomNumber(0,3);
+
+
+const MAX_COMMENT_LENGTH = 140;
+
+let checkLineLength = function (text, max) {
+  if  (text.length < max) {
+    return true;
+  }
+
+  return false;
+};
+
+checkLineLength('Текст', MAX_COMMENT_LENGTH);
+
