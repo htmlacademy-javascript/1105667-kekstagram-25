84939a648575a2bceac4ed6bebccfe3d3a1b8bdf
Keks <keks@htmlacademy.ru>
2022-03-16 23:18:09 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/data.js
+++ 5/js/data.js
@@ -27,23 +27,51 @@ const MESSAGES = [
 
 
 const comments = [];
+// Создание комментариев с одним комментарием
+// function createComment () {
+//   const commentRangeIds = [ ...Array(10000).keys() ].map( (i) => i+1); // массив всех чисел в заданном диапазоне
+//   const commentIds = []; // массив случайных неповторяющихся 25 чисел из массива выше
 
+
+//   for (let i = 0; i < 25; i++) {
+//     commentIds[i] = commentRangeIds.splice(getRandomNumber(0, commentRangeIds.length - 1),1)[0];
+//   }
+
+//   for (let i = 0; i < 25; i++) {
+//     comments[i] = [{
+//       id: commentIds[i],
+//       avatar: `img/avatar-${  getRandomNumber(1, 6)  }.svg`,
+//       message: getRandomArrayElement(MESSAGES),
+//       name: getRandomArrayElement(NAMES)
+//     }];
+//   }
+// }
+
+
+// Создание комментариев с несколькими комментариями
 function createComment () {
   const commentRangeIds = [ ...Array(10000).keys() ].map( (i) => i+1); // массив всех чисел в заданном диапазоне
-  const commentIds = []; // массив случайных неповторяющихся 25 чисел из массива выше
+  const commentIds = []; // массив куда будем записывать случайные неповторяющиеся числа из массива выше
 
 
-  for (let i = 0; i < 25; i++) {
+  for (let i = 0; i < 100; i++) {
     commentIds[i] = commentRangeIds.splice(getRandomNumber(0, commentRangeIds.length - 1),1)[0];
   }
-
-  for (let i = 0; i < 25; i++) {
-    comments[i] = [{
+  // Создаем библиотеку случайных комментариев
+  const commentsBlock = [];
+  for (let i = 0; i < 100; i++) {
+    commentsBlock[i] = {
       id: commentIds[i],
       avatar: `img/avatar-${  getRandomNumber(1, 6)  }.svg`,
       message: getRandomArrayElement(MESSAGES),
       name: getRandomArrayElement(NAMES)
-    }];
+    };
+  }
+
+
+  // Создаем 25 блоков комментариев из нескольких комментариев
+  for (let i = 0; i <25; i++) {
+    comments[i] = commentsBlock.splice(getRandomNumber(0, commentsBlock.length - 1),getRandomNumber(1,4));
   }
 }
 
