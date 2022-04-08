d99edea4093166b9a873172fa4c92007d0776739
Keks <keks@htmlacademy.ru>
2022-04-08 22:27:29 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/util.js
@@ -0,0 +1,19 @@
+const MAX_COMMENT_LENGTH = 140;
+const checkLineLength = (text) => text.length <= MAX_COMMENT_LENGTH;
+
+const getRandomNumber = (min, max) => {
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  throw new Error('Ошибка. Неправильно заданный диапазон');
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+export {checkLineLength, getRandomNumber, getRandomArrayElement, isEscapeKey};
