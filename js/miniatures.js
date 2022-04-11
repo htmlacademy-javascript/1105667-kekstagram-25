fd10681e1d21043086775a272acbee65a8db8cfa
Senya2648 <printsksenia@gmail.com>
2022-04-12 00:23:48 +0500

    Фильтры

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,9 +1,22 @@
 import {openPopup} from './popup.js';
+import {getData} from './api.js';
+import {debounce, getRandomArrayElement} from './util.js';
 
 const picturesSection = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+// Фильтры и кнопки
+const filterSection = document.querySelector('.img-filters');
+const randomButton = filterSection.querySelector('#filter-random');
+const discussedButton = filterSection.querySelector('#filter-discussed');
+const defaultButton = filterSection.querySelector('#filter-default');
 
 const renderPicturesList = (photoMocks) => {
+
+  const picturesToRemove = picturesSection.querySelectorAll('.picture');
+  for (let i = 0; i < picturesToRemove.length; i++) {
+    picturesToRemove[i].remove();
+  }
+
   const picturesSectionFragment = document.createDocumentFragment();
 
   photoMocks.forEach((picture) => {
@@ -16,11 +29,63 @@ const renderPicturesList = (photoMocks) => {
 
   picturesSection.appendChild(picturesSectionFragment);
 
+  filterSection.classList.remove('img-filters--inactive');
+
   const pictures = document.querySelectorAll('.picture');
 
   for (let i = 0; i < pictures.length; i++) {
     openPopup(pictures[i], photoMocks[i]);
   }
 };
+
+// массив 10 случайных фотографий
+const getRandomPictures = (pictures) => {
+  const picturesRandom = [];
+
+  for (let i = 0; i < 10; i++) {
+    let randomElement = getRandomArrayElement(pictures);
+
+    while (picturesRandom.includes(randomElement)) {
+      randomElement = getRandomArrayElement(pictures);
+    }
+    picturesRandom.push(randomElement);
+  }
+  return picturesRandom;
+};
+
+const RERENDER_DELAY = 500;
+
+randomButton.addEventListener('click', debounce(() => {
+  randomButton.classList.add('img-filters__button--active');
+  defaultButton.classList.remove('img-filters__button--active');
+  discussedButton.classList.remove('img-filters__button--active');
+
+  getData((pictures) => {
+    const randomPictures = getRandomPictures(pictures);
+    renderPicturesList(randomPictures);
+  });
+},RERENDER_DELAY));
+
+defaultButton.addEventListener('click', debounce(() => {
+  defaultButton.classList.add('img-filters__button--active');
+  randomButton.classList.remove('img-filters__button--active');
+  discussedButton.classList.remove('img-filters__button--active');
+
+  getData((pictures) => {
+    renderPicturesList(pictures);
+  });
+},RERENDER_DELAY));
+
+discussedButton.addEventListener('click', debounce(() => {
+  discussedButton.classList.add('img-filters__button--active');
+  randomButton.classList.remove('img-filters__button--active');
+  defaultButton.classList.remove('img-filters__button--active');
+
+  getData((pictures) => {
+    const discussedPictures = pictures.slice();
+    discussedPictures.sort((pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length);
+    renderPicturesList(discussedPictures);
+  });
+}, RERENDER_DELAY));
 
 export {renderPicturesList};
--- js/util.js
+++ js/util.js
@@ -3,6 +3,20 @@ const checkLineLength = (text) => text.length <= MAX_COMMENT_LENGTH;
 
 const isEscapeKey = (evt) => evt.key === 'Escape';
 
+const getRandomNumber = (min, max) => {
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  throw new Error('Ошибка. Неправильно заданный диапазон');
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+
 const ALERT_SHOW_TIME = 5000;
 const showAlert = (message) => {
   const alertContainer = document.createElement('div');
@@ -94,4 +108,25 @@ const createErrorMessage = function () {
   document.addEventListener('click', onErrorMessageClickOnRandomArea);
 };
 
-export {checkLineLength, isEscapeKey, showAlert, createSuccessMessage, createErrorMessage};
+// Функция взята из интернета и доработана
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+export {checkLineLength, isEscapeKey, showAlert, createSuccessMessage, createErrorMessage, debounce, getRandomArrayElement};
