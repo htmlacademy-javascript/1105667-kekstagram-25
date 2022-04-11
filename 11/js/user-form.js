8329b3ca9a8641c94f2544f055ed753c4d1c588a
Keks <keks@htmlacademy.ru>
2022-04-11 22:25:32 +0300

    :heavy_check_mark: Сборка #11

---
+++ 11/js/user-form.js
@@ -0,0 +1,181 @@
+import {isEscapeKey, checkLineLength} from './util.js';
+import './slider.js';
+import {sendData} from './api.js';
+const uploadForm = document.querySelector('#upload-select-image');
+const uploadFile = uploadForm.querySelector('#upload-file');
+const uploadFormCloseElement = uploadForm.querySelector('#upload-cancel');
+
+const hashtagsInput = uploadForm.querySelector('[name="hashtags"]');
+const descriptionInput = uploadForm.querySelector('[name="description"]');
+
+const scaleSmaller = document.querySelector('.scale__control--smaller');
+const scaleBigger = document.querySelector('.scale__control--bigger');
+const scaleControl = document.querySelector('.scale__control--value');
+const submitButton = document.querySelector('.img-upload__submit');
+
+
+const onUploadFormEscKeydown = (evt) => {
+  if ((isEscapeKey(evt) && !document.querySelector('.error')) || (isEscapeKey(evt) && document.querySelector('.error.hidden'))) {
+    closeUploadForm();
+  }
+};
+
+const onInputFocusout = () => {
+  document.addEventListener('keydown', onUploadFormEscKeydown);
+};
+
+const onInputFocusin = () => {
+  document.removeEventListener('keydown', onUploadFormEscKeydown);
+};
+
+const openUploadForm = () => {
+  document.querySelector('.img-upload__overlay').classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.querySelector('.effect-level__slider').classList.add('hidden');
+  // Сброс стилей
+  document.querySelector('.img-upload__preview').querySelector('img').removeAttribute('style');
+  document.querySelector('.img-upload__preview').querySelector('img').removeAttribute('class');
+  // Масштаб изображения по умолчанию
+  scaleControl.value = '100%';
+  // Добавляем обработчики
+  document.addEventListener('keydown', onUploadFormEscKeydown);
+  uploadFormCloseElement.addEventListener('click', closeUploadForm);
+
+  hashtagsInput.addEventListener('focusin', onInputFocusin);
+  hashtagsInput.addEventListener('focusout', onInputFocusout);
+
+  descriptionInput.addEventListener('focusin', onInputFocusin);
+  descriptionInput.addEventListener('focusout', onInputFocusout);
+  // Добавляем обработчики кнопок масштаба
+  scaleSmaller.addEventListener('click', makeScaleSmaller);
+  scaleBigger.addEventListener('click', makeScaleBigger);
+
+};
+
+function closeUploadForm () {
+  document.querySelector('.img-upload__overlay').classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  // Сброс полей
+  uploadFile.value = '';
+  hashtagsInput.value = '';
+  descriptionInput.value = '';
+  // Убираем обработчики
+  document.removeEventListener('keydown', onUploadFormEscKeydown);
+  uploadFormCloseElement.removeEventListener('click', closeUploadForm);
+  hashtagsInput.removeEventListener('focusin', onInputFocusin);
+  hashtagsInput.removeEventListener('focusout', onInputFocusout);
+  descriptionInput.removeEventListener('focusin', onInputFocusin);
+  descriptionInput.removeEventListener('focusout', onInputFocusout);
+  // Убираем обработчики с кнопок масштаба
+  scaleSmaller.removeEventListener('click', makeScaleSmaller);
+  scaleBigger.removeEventListener('click', makeScaleBigger);
+  // Сброс стиля изображения
+  document.querySelector('.img-upload__preview').querySelector('img').style.transform = '';
+
+
+}
+
+uploadFile.addEventListener('change', openUploadForm);
+
+// Функции масштабирования изображения
+function makeScaleSmaller () {
+  if (scaleControl.value !== '25%') {
+    scaleControl.value = `${Number(scaleControl.value.slice(0,-1)) - 25  }%`;
+    document.querySelector('.img-upload__preview').querySelector('img').style.transform = `scale(${ scaleControl.value.slice(0,-1)/100})`;
+  }
+}
+
+function makeScaleBigger () {
+  if (scaleControl.value !== '100%') {
+    scaleControl.value = `${Number(scaleControl.value.slice(0,-1)) + 25  }%`;
+    document.querySelector('.img-upload__preview').querySelector('img').style.transform = `scale(${ scaleControl.value.slice(0,-1)/100})`;
+  }
+}
+
+// Валидация
+const pristine = new Pristine(uploadForm, {
+  classTo: 'text__item',
+  errorClass: 'text__item--invalid',
+  successClass: 'text__item--valid',
+  errorTextParent: 'text__item',
+  errorTextTag: 'p',
+  errorTextClass: 'text__error'
+},false);
+
+pristine.addValidator(descriptionInput, checkLineLength,'- длина комментария не может составлять больше 140 символов;');
+pristine.addValidator(hashtagsInput, checkHashtags, getHashtagsErrorMessage);
+
+function checkHashtags () {
+  if (hashtagsInput.value === '') {
+    return true;
+  }
+  const userHashtags = hashtagsInput.value.split(' ');
+  const userHashtagsLowerCase = userHashtags.map((element) => element.toLowerCase());
+
+  return checkHashtagsNotation(userHashtags) && checkHashtagsQuantity(userHashtags) && checkHashtagsUniqueness(userHashtagsLowerCase);
+}
+
+function checkHashtagsNotation (hashtags) {
+  const re = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+  for (let i = 0; i < hashtags.length; i++) {
+    if (!re.test(hashtags[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkHashtagsQuantity (hashtags) {
+  return hashtags.length <=5;
+}
+
+
+function checkHashtagsUniqueness (hashtags) {
+  const uniqueHashtags = new Set(hashtags);
+  return hashtags.length === uniqueHashtags.size;
+}
+
+function getHashtagsErrorMessage () {
+  return '<p>&mdash; хэш-тег начинается с символа # (решётка);</p><p>&mdash; строка после решётки должна состоять из букв и чисел и не может содержать пробелы, спецсимволы (#, @, $ и т. п.), символы пунктуации (тире, дефис, запятая и т. п.), эмодзи и т. д.; </p><p>&mdash; хеш-тег не может состоять только из одной решётки;</p><p>&mdash; максимальная длина одного хэш-тега 20 символов, включая решётку;</p><p>&mdash; хэш-теги нечувствительны к регистру: #ХэшТег и #хэштег считаются одним и тем же тегом;</p><p>&mdash; хэш-теги разделяются пробелами;</p><p>&mdash; один и тот же хэш-тег не может быть использован дважды;</p><p>&mdash; нельзя указать больше пяти хэш-тегов;</p>';
+
+}
+
+// Блокировка и разблокировка кнопки
+const blockSubmitButton = () => {
+  submitButton.disabled = true;
+  submitButton.textContent = 'Публикую...';
+};
+
+const unblockSubmitButton = () => {
+  submitButton.disabled = false;
+  submitButton.textContent = 'Опубликовать';
+};
+
+// Проверка на валидность с отменой отправки
+
+const setUserFormSubmit = (onSuccess) => {
+  uploadForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+
+    const isValid = pristine.validate();
+    if (isValid) {
+      blockSubmitButton();
+      sendData(
+        () => {
+          onSuccess();
+          unblockSubmitButton();
+        },
+        () => {
+          unblockSubmitButton();
+          // document.querySelector('.img-upload__overlay').classList.add('hidden');
+        },
+        new FormData(evt.target),
+      );
+    }
+    if (!isValid) {
+      evt.preventDefault();
+    }
+  });
+};
+
+export {closeUploadForm, setUserFormSubmit};
---
+++ 11/js/util.js
@@ -0,0 +1,132 @@
+const MAX_COMMENT_LENGTH = 140;
+const checkLineLength = (text) => text.length <= MAX_COMMENT_LENGTH;
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const getRandomNumber = (min, max) => {
+  if (min >= 0 && max > min) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  throw new Error('Ошибка. Неправильно заданный диапазон');
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomNumber(0, elements.length - 1)];
+
+
+const ALERT_SHOW_TIME = 5000;
+const showAlert = (message) => {
+  const alertContainer = document.createElement('div');
+  alertContainer.style.zIndex = 100;
+  alertContainer.style.position = 'absolute';
+  alertContainer.style.left = 0;
+  alertContainer.style.top = 0;
+  alertContainer.style.right = 0;
+  alertContainer.style.padding = '10px 3px';
+  alertContainer.style.fontSize = '20px';
+  alertContainer.style.textAlign = 'center';
+  alertContainer.style.textTransform = 'none';
+  alertContainer.style.backgroundColor = 'red';
+
+  alertContainer.textContent = message;
+
+  document.body.append(alertContainer);
+
+  setTimeout(() => {
+    alertContainer.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+// Шаблоны успех и ошибка
+const succesTemplate = document.querySelector('#success').content.querySelector('.success');
+const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+const body = document.body;
+
+const succesElement = succesTemplate.cloneNode(true);
+const successButton = succesElement.querySelector('.success__button');
+
+const onSuccessMessageEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    succesElement.classList.add('hidden');
+    document.removeEventListener('keydown', onSuccessMessageEscKeydown);
+  }
+};
+
+const closeSuccessMessage = function () {
+  succesElement.classList.add('hidden');
+  successButton.removeEventListener('click', closeSuccessMessage);
+};
+
+const onSuccessMessageClickOnRandomArea = function (evt) {
+  if (evt.target.className === 'success') {
+    succesElement.classList.add('hidden');
+  }
+};
+
+const createSuccessMessage = function () {
+  body.appendChild(succesElement);
+
+  succesElement.classList.remove('hidden');
+  successButton.addEventListener('click', closeSuccessMessage);
+  document.addEventListener('keydown', onSuccessMessageEscKeydown);
+  document.addEventListener('click', onSuccessMessageClickOnRandomArea);
+};
+
+const errorElement = errorTemplate.cloneNode(true);
+const errorButton = errorElement.querySelector('.error__button');
+
+const onErrorMessageEscKeydown = (evt) => {
+  if (isEscapeKey(evt)) {
+    errorElement.classList.add('hidden');
+    document.removeEventListener('keydown', onErrorMessageEscKeydown);
+  }
+};
+
+const onErrorMessageClickOnRandomArea = function (evt) {
+  if (evt.target.className === 'error') {
+    errorElement.classList.add('hidden');
+  }
+  document.removeEventListener('click', onErrorMessageClickOnRandomArea);
+};
+
+const closeErrorMessage = function () {
+  errorElement.classList.add('hidden');
+  errorButton.removeEventListener('click', closeErrorMessage);
+};
+
+const createErrorMessage = function () {
+  body.appendChild(errorElement);
+
+  errorElement.classList.remove('hidden');
+  errorElement.style.zIndex = '3';
+  errorButton.addEventListener('click', closeErrorMessage);
+  document.addEventListener('keydown', onErrorMessageEscKeydown);
+  document.addEventListener('click', onErrorMessageClickOnRandomArea);
+};
+
+// Функция взята из интернета и доработана
+// Источник - https://www.freecodecamp.org/news/javascript-debounce-example
+
+function debounce (callback, timeoutDelay = 500) {
+  // Используем замыкания, чтобы id таймаута у нас навсегда приклеился
+  // к возвращаемой функции с setTimeout, тогда мы его сможем перезаписывать
+  let timeoutId;
+
+  return (...rest) => {
+    // Перед каждым новым вызовом удаляем предыдущий таймаут,
+    // чтобы они не накапливались
+    clearTimeout(timeoutId);
+
+    // Затем устанавливаем новый таймаут с вызовом колбэка на ту же задержку
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+
+    // Таким образом цикл «поставить таймаут - удалить таймаут» будет выполняться,
+    // пока действие совершается чаще, чем переданная задержка timeoutDelay
+  };
+}
+
+export {checkLineLength, isEscapeKey, showAlert, createSuccessMessage, createErrorMessage, debounce, getRandomArrayElement};
