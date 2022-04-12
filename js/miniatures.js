c4917d398220b9b3090bc23e8ea6f834e346a706
Senya2648 <printsksenia@gmail.com>
2022-04-12 23:42:15 +0500

    Правки 2022.04.12

--- js/miniatures.js
+++ js/miniatures.js
@@ -1,16 +1,13 @@
 import {openPopup} from './popup.js';
-import {getData} from './api.js';
-import {debounce, getRandomArrayElement} from './util.js';
+import {getRandomArrayElement} from './util.js';
 
 const picturesSection = document.querySelector('.pictures');
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
-// Фильтры и кнопки
+// Фильтры
 const filterSection = document.querySelector('.img-filters');
-const randomButton = filterSection.querySelector('#filter-random');
-const discussedButton = filterSection.querySelector('#filter-discussed');
-const defaultButton = filterSection.querySelector('#filter-default');
 
-const renderPicturesList = (photoMocks) => {
+
+const renderPicturesList = (photos) => {
 
   const picturesToRemove = picturesSection.querySelectorAll('.picture');
   for (let i = 0; i < picturesToRemove.length; i++) {
@@ -19,7 +16,7 @@ const renderPicturesList = (photoMocks) => {
 
   const picturesSectionFragment = document.createDocumentFragment();
 
-  photoMocks.forEach((picture) => {
+  photos.forEach((picture) => {
     const pictureElement = pictureTemplate.cloneNode(true);
     pictureElement.querySelector('.picture__img').src = picture.url;
     pictureElement.querySelector('.picture__likes').textContent = picture.likes;
@@ -34,7 +31,7 @@ const renderPicturesList = (photoMocks) => {
   const pictures = document.querySelectorAll('.picture');
 
   for (let i = 0; i < pictures.length; i++) {
-    openPopup(pictures[i], photoMocks[i]);
+    openPopup(pictures[i], photos[i]);
   }
 };
 
@@ -53,39 +50,31 @@ const getRandomPictures = (pictures) => {
   return picturesRandom;
 };
 
-const RERENDER_DELAY = 500;
-
-randomButton.addEventListener('click', debounce(() => {
-  randomButton.classList.add('img-filters__button--active');
-  defaultButton.classList.remove('img-filters__button--active');
-  discussedButton.classList.remove('img-filters__button--active');
-
-  getData((pictures) => {
-    const randomPictures = getRandomPictures(pictures);
-    renderPicturesList(randomPictures);
-  });
-},RERENDER_DELAY));
-
-defaultButton.addEventListener('click', debounce(() => {
-  defaultButton.classList.add('img-filters__button--active');
-  randomButton.classList.remove('img-filters__button--active');
-  discussedButton.classList.remove('img-filters__button--active');
-
-  getData((pictures) => {
-    renderPicturesList(pictures);
-  });
-},RERENDER_DELAY));
-
-discussedButton.addEventListener('click', debounce(() => {
-  discussedButton.classList.add('img-filters__button--active');
-  randomButton.classList.remove('img-filters__button--active');
-  defaultButton.classList.remove('img-filters__button--active');
-
-  getData((pictures) => {
+const drawPictures = (evt, pictures) => {
+  const targetBtn = evt.target;
+  let picturesToShow = pictures;
+  if (targetBtn.id === 'filter-random') {
+    picturesToShow = getRandomPictures(pictures);
+  }
+  if (targetBtn.id === 'filter-discussed') {
     const discussedPictures = pictures.slice();
     discussedPictures.sort((pictureA, pictureB) => pictureB.comments.length - pictureA.comments.length);
-    renderPicturesList(discussedPictures);
-  });
-}, RERENDER_DELAY));
+    picturesToShow = discussedPictures;
+  }
+  renderPicturesList(picturesToShow);
+};
 
-export {renderPicturesList};
+const setFiltersClick = (cb) => {
+  document.querySelector('.img-filters__form').addEventListener('click', (evt) => {
+    const targetBtn = evt.target;
+    const activeBtn = document.querySelector('.img-filters__button--active');
+    if (targetBtn !== activeBtn) {
+      activeBtn.classList.remove('img-filters__button--active');
+      targetBtn.classList.add('img-filters__button--active');
+
+      cb(evt);
+    }
+  });
+};
+
+export {renderPicturesList, drawPictures, setFiltersClick};
