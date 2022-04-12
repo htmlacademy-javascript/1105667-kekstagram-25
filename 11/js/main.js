d926e7e7cd8575877931d1d91b1e4020400b2796
Keks <keks@htmlacademy.ru>
2022-04-12 21:44:35 +0300

    :heavy_check_mark: Сборка #11

--- 11/js/main.js
+++ 11/js/main.js
@@ -1,11 +1,18 @@
 import './miniatures.js';
 import './user-form.js';
+import {debounce} from './util.js';
 import {closeUploadForm, setUserFormSubmit} from './user-form.js';
-import {renderPicturesList} from './miniatures.js';
+import {renderPicturesList, drawPictures, setFiltersClick} from './miniatures.js';
 import {getData} from './api.js';
+
+const RERENDER_DELAY = 500;
 
 getData((pictures) => {
   renderPicturesList(pictures);
+  setFiltersClick(debounce(
+    (evt) => drawPictures(evt, pictures),
+    RERENDER_DELAY,
+  ));
 });
 
 setUserFormSubmit(closeUploadForm);
